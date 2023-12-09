4e5cba5825315fe715d36aa754371fdb5c3eccb4
謝志謙 <twcch1218@gmail.com>
2023-12-09 21:21:22 +0800

    update

--- TestJava/src/com/twcch/testjava/algorithm/BinarySearch.java
+++ TestJava/src/com/twcch/testjava/algorithm/BinarySearch.java
@@ -1,12 +1,12 @@
 package com.twcch.testjava.algorithm;
 
-public class TestDemo {
+public class BinarySearch {
 
 	public static void main(String[] args) {
 
 		int[] nums = { -1, 0, 3, 5, 9, 12 };
 
-		TestDemo t = new TestDemo();
+		BinarySearch t = new BinarySearch();
 		int result = t.search(nums, 9);
 
 		System.out.println("result: " + result);
