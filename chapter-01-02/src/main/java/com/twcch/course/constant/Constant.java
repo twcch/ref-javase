9e6ddc0ee8e4f2772e1efcfa11379c6889b866d5
謝志謙 <twcch1218@gmail.com>
2023-12-09 10:24:27 +0800

    更新 Chapter-01-02

---
+++ chapter-01-02/src/main/java/com/twcch/course/constant/Constant.java
@@ -0,0 +1,13 @@
+package com.twcch.course.constant;
+
+public class Constant {
+
+    public static double PI = 3.14159;
+
+    public static void main(String[] args) {
+
+        System.out.println("PI: " + PI);
+
+    }
+
+}
---
+++ chapter-01-02/src/main/java/com/twcch/course/variable/PrimitiveDataType.java
@@ -0,0 +1,25 @@
+package com.twcch.course.variable;
+
+public class PrimitiveDataType {
+
+    public static void main(String[] args) {
+
+        /*
+         * byte => short, char => int => long => float => double
+         * boolean
+         */
+        byte a = 127;
+        short b = 10000;
+        char c = 100;
+        int d = 1234;
+        long e = 1000L;
+        float f = 3.14f;
+        double g = 3.14;
+        boolean h = true;
+
+        System.out.printf("a = %5d, b = %5d, c = %5c, d = %5d, e = %5d, " +
+                "f = %5f, g = %5f, h = %b", a, b, c, d, e, f, g, h);
+
+    }
+
+}
