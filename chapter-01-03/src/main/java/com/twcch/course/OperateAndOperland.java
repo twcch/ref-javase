36f54d42990cec91abf55e800ad86d39966fe8f7
謝志謙 <twcch1218@gmail.com>
2023-12-09 15:37:55 +0800

    更新 Chapter-01-03

---
+++ chapter-01-03/src/main/java/com/twcch/course/OperateAndOperland.java
@@ -0,0 +1,17 @@
+package com.twcch.course;
+
+public class OperateAndOperland {
+
+	public static void main(String[] args) {
+
+		int a = 10;
+		int b = 3;
+
+		System.out.println("a + b = " + (a + b));
+		System.out.println("a - b = " + (a - b));
+		System.out.println("a * b = " + (a * b));
+		System.out.println("a / b = " + (a / b));
+
+	}
+
+}
