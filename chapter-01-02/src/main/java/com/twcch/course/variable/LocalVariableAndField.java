da1c7088f133e8b3dc534f245d7f79611c6f21cb
謝志謙 <twcch1218@gmail.com>
2023-12-09 15:07:50 +0800

    更新 Chapter-01-02

---
+++ chapter-01-02/src/main/java/com/twcch/course/variable/LocalVariableAndField.java
@@ -0,0 +1,17 @@
+package com.twcch.course.variable;
+
+public class LocalVariableAndField {
+
+    public static int a;
+
+    public static void main(String[] args) {
+
+        System.out.println("a = " + a);
+
+        int a = 10;
+
+        System.out.println("a = " + a);
+
+    }
+
+}
