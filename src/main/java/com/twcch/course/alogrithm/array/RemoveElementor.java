a5868a2132d721a12d040abed0190b5424ff44df
謝志謙 <twcch1218@gmail.com>
2023-12-10 16:21:56 +0800

    update

---
+++ src/main/java/com/twcch/course/alogrithm/array/RemoveElementor.java
@@ -0,0 +1,35 @@
+package com.twcch.course.alogrithm.array;
+
+import java.util.Arrays;
+
+public class RemoveElementor {
+
+	public static void main(String[] args) {
+
+		int[] nums = { 3, 2, 2, 3 };
+		int val = 3;
+
+		RemoveElementor t = new RemoveElementor();
+		int result = t.removeElementor(nums, val);
+
+		System.out.println("result: " + result);
+
+	}
+
+	public int removeElementor(int[] nums, int val) {
+
+		int slowIndex = 0;
+
+		for (int fastIndex = 0; fastIndex < nums.length; fastIndex++) {
+			if (nums[fastIndex] != val) {
+				nums[slowIndex++] = nums[fastIndex];
+			}
+		}
+		
+		System.out.println("nums: " + Arrays.toString(nums));
+
+		return slowIndex;
+
+	}
+
+}
