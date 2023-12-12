4828d106b48c4436bb629c3e80d4779523f463c3
謝志謙 <twcch1218@gmail.com>
2023-12-12 22:21:20 +0800

    update

--- src/main/java/com/twcch/course/basic/variableandconstant/LocalVariableAndField.java
+++ src/main/java/com/twcch/course/basic/variableandconstant/LocalVariableAndField.java
@@ -2,13 +2,13 @@ package com.twcch.course.basic.variableandconstant;
 
 public class LocalVariableAndField {
 
-	public static double PI = 3.14159;
+	public static final double PI = 3.14159;
 
 	public static void main(String[] args) {
 
 		System.out.println("PI: " + PI);
 
-		double PI = 3.14;
+		final double PI = 3.14;
 
 		System.out.println("PI: " + PI);
 
