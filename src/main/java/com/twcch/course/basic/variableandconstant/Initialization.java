fc355ce7ba72f030ef48d768dfba14bfbdb5bdbd
謝志謙 <twcch1218@gmail.com>
2023-12-12 22:27:35 +0800

    update

---
+++ src/main/java/com/twcch/course/basic/variableandconstant/Initialization.java
@@ -0,0 +1,19 @@
+package com.twcch.course.basic.variableandconstant;
+
+public class Initialization {
+
+    public static void main(String[] args) {
+
+        int a, b = 2;
+        a = 1;
+
+        System.out.printf("a = %d, b = %d", a, b);
+
+        final double PI;
+        PI = 3.14;
+
+        System.out.printf("\nPI = %.2f", PI);
+
+    }
+
+}
