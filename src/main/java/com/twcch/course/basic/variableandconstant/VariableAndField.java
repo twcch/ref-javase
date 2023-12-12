b325d54c5fafa24a580b9769cfdb91893933e823
謝志謙 <twcch1218@gmail.com>
2023-12-12 22:33:08 +0800

    update

---
+++ src/main/java/com/twcch/course/basic/variableandconstant/VariableAndField.java
@@ -0,0 +1,21 @@
+package com.twcch.course.basic.variableandconstant;
+
+public class VariableAndField {
+
+	public final double NEW_PI = 3.141; // field: instance variable
+	public static final double PI = 3.14159; // field: static variable
+
+	public static void main(String[] args) {
+
+		System.out.println("PI: " + PI);
+
+		final double PI = 3.14; // local variable
+
+		System.out.println("PI: " + PI);
+
+		VariableAndField v = new VariableAndField();
+		System.out.println("NEW_PI: " + v.NEW_PI);
+
+	}
+
+}
