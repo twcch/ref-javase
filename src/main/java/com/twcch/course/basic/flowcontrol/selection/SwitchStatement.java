515858c4428508590037c37bdfd80e24dca82c25
謝志謙 <twcch1218@gmail.com>
2023-12-14 21:57:56 +0800

    update

--- src/main/java/com/twcch/course/basic/flowcontrol/selection/SwitchStatement.java
+++ src/main/java/com/twcch/course/basic/flowcontrol/selection/SwitchStatement.java
@@ -3,9 +3,25 @@ package com.twcch.course.basic.flowcontrol.selection;
 public class SwitchStatement {
 
 	public static void main(String[] args) {
-		
-		
-		
+
+		/* 
+		 * integer: byte, short, char, int
+		 * String
+		 * Enum
+		 */
+		int a = 1;
+
+		switch (a) {
+		case 1:
+			System.out.println("1");
+			break;
+		case 2:
+			System.out.println("2");
+			break;
+		default:
+			break;
+		}
+
 	}
 
 }
---
+++ src/main/java/com/twcch/course/basic/flowcontrol/selection/TernaryOperator.java
@@ -0,0 +1,14 @@
+package com.twcch.course.basic.flowcontrol.selection;
+
+public class TernaryOperator {
+
+	public static void main(String[] args) {
+		
+		int a = 10;
+		int b = 5;
+		
+		System.out.println("result: " + (a > b ? "a" : "b"));
+		
+	}
+
+}
