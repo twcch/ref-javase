7f25c9b044628d84d3579e67dabcfaea97f051f4
謝志謙 <twcch1218@gmail.com>
2023-12-14 20:09:04 +0800

    update

--- src/main/java/com/twcch/course/basic/HelloWorld.java
+++ src/main/java/com/twcch/course/basic/HelloWorld.java
@@ -5,12 +5,6 @@ public class HelloWorld {
 	public static void main(String[] args) {
 
 		System.out.println("Hello world!");
-		
-		int a = 8;
-		int sum = 6;
-		sum =+ a * 2; // sum = +a * 2
-		
-		System.out.println(sum);
 
 	}
 
