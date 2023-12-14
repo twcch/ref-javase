3170ecf0ed358fcd4fd2688f3dd7c1edca74a12a
謝志謙 <twcch1218@gmail.com>
2023-12-14 22:02:15 +0800

    update

--- src/main/java/com/twcch/course/basic/flowcontrol/selection/SwitchStatement.java
+++ src/main/java/com/twcch/course/basic/flowcontrol/selection/SwitchStatement.java
@@ -4,24 +4,31 @@ public class SwitchStatement {
 
 	public static void main(String[] args) {
 
+		String str = "A";
+		
+		SwitchStatement s = new SwitchStatement();
+		boolean result = s.isA(str);
+		
+		System.out.println(result);
+
+	}
+
+	public boolean isA(String s) {
+		boolean result = false;
+
 		/* 
 		 * integer: byte, short, char, int
 		 * String
 		 * Enum
 		 */
-		int a = 1;
-
-		switch (a) {
-		case 1:
-			System.out.println("1");
-			break;
-		case 2:
-			System.out.println("2");
-			break;
+		switch (s) {
+		case "A":
+			result = true;
 		default:
 			break;
 		}
 
+		return result;
 	}
 
 }
