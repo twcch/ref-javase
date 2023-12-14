9765fe1231ab5bf7202309eef313b2f12b77162e
謝志謙 <twcch1218@gmail.com>
2023-12-14 22:54:52 +0800

    update

--- src/main/java/com/twcch/course/basic/HelloWorld.java
+++ src/main/java/com/twcch/course/basic/HelloWorld.java
@@ -6,21 +6,6 @@ public class HelloWorld {
 
 		System.out.println("Hello world!");
 
-		String str = "";
-
-		z: for (int x = 0; x < 3; x++) {
-			for (int y = 0; y < 2; y++) {
-				if (x == 1)
-					break;
-				if (y == 2)
-					break z;
-				str = str + x + y;
-				System.out.printf("str: %10s, x: %d, y: %d \n", str, x, y);
-			}
-		}
-
-		System.out.println(str);
-
 	}
 
 }
