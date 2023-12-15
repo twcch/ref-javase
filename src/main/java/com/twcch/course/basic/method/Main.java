ed4e23aa645a202096e2fdd8d3a7c54b845ad311
謝志謙 <twcch1218@gmail.com>
2023-12-15 21:55:19 +0800

    update

--- src/main/java/com/twcch/course/basic/method/Main.java
+++ src/main/java/com/twcch/course/basic/method/Main.java
@@ -6,7 +6,7 @@ public class Main {
 		
 		String msg = "Hello Java!";
 		
-		Printer printer = new Printer();
+		Printer printer = new Printer(2);
 		printer.print(msg, 3);
 
 	}
--- src/main/java/com/twcch/course/basic/method/Printer.java
+++ src/main/java/com/twcch/course/basic/method/Printer.java
@@ -2,13 +2,41 @@ package com.twcch.course.basic.method;
 
 public class Printer {
 
-	public void print(String message, int times) {
+	private int count = 0;
 
-		while (times > 0) {
-			System.out.println("Printer: " + message);
-			times--;
+	public Printer(int count) {
+		this.count = count;
+	}
+
+	public void print(String msg) {
+
+		if (count <= 0) {
+			return;
 		}
 
+		System.out.println("Printer: " + msg);
+
+	}
+
+	public void print(String msg, int times) { // overloading
+
+		while (times > 0) {
+			if (count <= 0) {
+				return;
+			}
+
+			System.out.println("Printer: " + msg);
+			count--;
+		}
+
+	}
+
+	public int getCount() {
+		return count;
+	}
+
+	public void setCount(int count) {
+		this.count = count;
 	}
 
 }
