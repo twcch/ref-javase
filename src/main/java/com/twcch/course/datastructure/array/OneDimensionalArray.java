2f022b355eb2ae802e80b3b657bf4680ec9f67cf
謝志謙 <twcch1218@gmail.com>
2023-12-16 16:33:45 +0800

    update

--- src/main/java/com/twcch/course/datastructure/array/OneDimensionalArray.java
+++ src/main/java/com/twcch/course/datastructure/array/OneDimensionalArray.java
@@ -6,7 +6,7 @@ public class OneDimensionalArray {
 
 	public static void main(String[] args) {
 
-		int[] arr1 = new int[] { 1, 2, 3 };
+		int[] arr1 = new int[] { 1, 2, 3 }; // array 為連續、長度不可變的
 		System.out.println("arr1: " + Arrays.toString(arr1));
 
 		int[] arr2 = { 1, 2, 3 };
