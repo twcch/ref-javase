c7de4d2dc041b5d19fab48e15d1f9bac7ceeb535
謝志謙 <twcch1218@gmail.com>
2023-12-16 23:37:53 +0800

    update

---
+++ src/main/java/com/twcch/course/library/arrays/Main.java
@@ -0,0 +1,28 @@
+package com.twcch.course.library.arrays;
+
+import java.util.Arrays;
+
+public class Main {
+
+	public static void main(String[] args) {
+
+		int[] arr1 = new int[] { 3, 4, 1, 2, 5 };
+		System.out.println("arr1: " + Arrays.toString(arr1)); // 將一維陣列的所有元素轉成字串
+		
+		Arrays.sort(arr1); // 排序陣列
+		System.out.println("arr1: " + Arrays.toString(arr1));
+		
+		int search = 4;
+		int index = Arrays.binarySearch(arr1, search); // 二分查找法 (需排序後)
+		System.out.println("arr1 search: " + search + ", result index: " + index);
+		
+		int[] newArr1 = Arrays.copyOf(arr1, 9); // 複製陣列
+		System.out.println("arr1: " + Arrays.toString(arr1));
+		System.out.println("new arr1: " + Arrays.toString(newArr1));
+
+		int[][] arr2 = new int[][] { { 3, 4, 1 }, { 2, 5 } };
+		System.out.println("arr2: " + Arrays.deepToString(arr2)); // 將二維陣列的所有元素轉成字串
+
+	}
+
+}
