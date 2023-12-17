10badc4520989319362dcfcd1b94061290eda405
謝志謙 <twcch1218@gmail.com>
2023-12-17 20:46:58 +0800

    update

--- src/main/java/com/twcch/course/oop/staticmodify/Info.java
+++ src/main/java/com/twcch/course/oop/staticmodify/Info.java
@@ -3,8 +3,9 @@ package com.twcch.course.oop.staticmodify;
 public class Info {
 	
 	/* 
-	 * 可以被透過類別或物件進行呼叫，但透過類別呼叫較為合理
-	 * 儲存在 global 給所有人共用，只有一份
+	 * static:
+	 * 1. 當類別被初始化後，會儲存在 global 給所有人共用，只有一份
+	 * 2. 可以被透過類別或物件進行呼叫，但透過類別呼叫較為合理
 	 */
 	public static final String MSG = "Hello Java!";
 	
