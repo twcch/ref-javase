9a83fd76f8e84fa30674fdb8f7b99188800f803b
謝志謙 <twcch1218@gmail.com>
2023-12-17 21:29:43 +0800

    update

--- src/main/java/com/twcch/course/oop/constructor/MyInfo.java
+++ src/main/java/com/twcch/course/oop/constructor/MyInfo.java
@@ -10,6 +10,7 @@ public class MyInfo {
 	 * 2. constructor 沒有回傳型別
 	 * 3. 若 class 沒有 constructor: Java 會預設建立一個無參的 constructor，但該類別只要有任一個 constructor: Java 就不會自動建立 constructor
 	 * 4. constructor 第一行預設呼叫 super() 的方法
+	 * 5. constructor 無法被繼承
 	 */
 	public MyInfo() { // 無參建構子
 
--- src/main/java/com/twcch/course/oop/inheritance/SubClass.java
+++ src/main/java/com/twcch/course/oop/inheritance/SubClass.java
@@ -1,6 +1,6 @@
 package com.twcch.course.oop.inheritance;
 
-public class SubClass extends SuperClass {
+public class SubClass extends SuperClass { // SubClass is a SuperClass，若無預設繼承 Object
 	
 	
 	
