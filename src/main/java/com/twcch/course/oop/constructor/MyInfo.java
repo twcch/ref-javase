3dcb07b06e2406659ac035bb545634cdf51bf654
謝志謙 <twcch1218@gmail.com>
2023-12-17 10:32:32 +0800

    update

--- src/main/java/com/twcch/course/oop/constructor/MyInfo.java
+++ src/main/java/com/twcch/course/oop/constructor/MyInfo.java
@@ -8,7 +8,7 @@ public class MyInfo {
 	 * constructor:
 	 * 1. constructor 名稱一定要與 class 一致
 	 * 2. constructor 沒有回傳型別
-	 * 3. 若 class 沒有 constructor 系統會預設建立一個無參的 constructor
+	 * 3. 若 class 沒有 constructor: Java 會預設建立一個無參的 constructor，但該類別只要有任一個 constructor: Java 就不會自動建立 constructor
 	 * 4. constructor 第一行預設呼叫 super() 的方法
 	 */
 	public MyInfo() { // 無參建構子
