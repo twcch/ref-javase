72d7a7fbd0bae8383b25041ad6c3ba875258eb5b
謝志謙 <twcch1218@gmail.com>
2023-12-17 10:30:38 +0800

    update

---
+++ src/main/java/com/twcch/course/oop/constructor/MyInfo.java
@@ -0,0 +1,26 @@
+package com.twcch.course.oop.constructor;
+
+public class MyInfo {
+	
+	private String info;
+	
+	/* 
+	 * constructor:
+	 * 1. constructor 名稱一定要與 class 一致
+	 * 2. constructor 沒有回傳型別
+	 * 3. 若 class 沒有 constructor 系統會預設建立一個無參的 constructor
+	 * 4. constructor 第一行預設呼叫 super() 的方法
+	 */
+	public MyInfo() { // 無參建構子
+		
+	}
+	
+	public MyInfo(String info) { // 有參建構子
+		this.info = info;
+	}
+	
+	public String getInfo() {
+		return info;
+	}
+
+}
---
+++ src/main/java/com/twcch/course/oop/constructor/MyMsg.java
@@ -0,0 +1,11 @@
+package com.twcch.course.oop.constructor;
+
+public class MyMsg {
+	
+	public static final String HELLO = "hello";
+	
+	private MyMsg() { // 私有的建構子，表示無法建立物件
+		
+	}
+	
+}
