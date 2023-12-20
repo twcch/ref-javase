4eeb96e8c4c858950d8e613780d0ee3879cb2b32
謝志謙 <twcch1218@gmail.com>
2023-12-20 22:38:56 +0800

    update

---
+++ src/main/java/com/twcch/course/oop/interfaceclass/Main.java
@@ -0,0 +1,12 @@
+package com.twcch.course.oop.interfaceclass;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		SuperClass sc = new SubClass();
+		sc.printString("Hello");
+
+	}
+
+}
---
+++ src/main/java/com/twcch/course/oop/interfaceclass/SubClass.java
@@ -0,0 +1,12 @@
+package com.twcch.course.oop.interfaceclass;
+
+public class SubClass implements SuperClass {
+
+	@Override
+	public void printString(String s) {
+		
+		System.out.println(s);
+		
+	}
+
+}
