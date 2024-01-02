fef475f7d21d988a4b27a7c8b0a98645f57a8e2d
謝志謙 <twcch1218@gmail.com>
2024-01-02 20:58:29 +0800

    update

--- src/main/java/com/twcch/refjavase/HelloWorld.java
+++ src/main/java/com/twcch/refjavase/HelloWorld.java
@@ -1,14 +1,10 @@
 package com.twcch.refjavase;
 
-import java.time.Instant;
-
 public class HelloWorld {
 	
 	public static void main(String[] args) {
 		
-		Instant time = Instant.now();
-		
-		System.out.println(time);
+		System.out.println("Hello World!");
 		
 	}
 	
---
+++ src/main/java/com/twcch/refjavase/application/datetime/InstantClass.java
@@ -0,0 +1,15 @@
+package com.twcch.refjavase.application.datetime;
+
+import java.time.Instant;
+
+public class InstantClass {
+
+	public static void main(String[] args) {
+		
+		Instant now = Instant.now();
+		
+		System.out.println("現在時間: " + now);;
+
+	}
+
+}
